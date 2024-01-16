4bc5d2dea6a115b5968f2202b5538b022f6760f5
samarqureshii <samarq713@gmail.com>
2024-01-16 11:37:39 -0500

    Annie's commit

--- lab1/part3.s
+++ lab1/part3.s
@@ -1,27 +1,18 @@
-
-/*Write a Nios II Assembly language program that computes the sum of the numbers from 1 to 30,
-; in a loop that explicitly computes the sum (i.e. don’t use a formula to compute the sum, use a loop). 
-; The sum must be placed into register r12 when the program is finished, and the program should go into 
-; an infinite loop when it is done, as shown in Figure 1 at the bottom. Test your program by compiling and running it on CPUlator.
-
-; one 5 bit register to be the iterator
-; another register to store the sum */
-
-
 .global _start
-_start:
-    movi r8, 0  /*we will use r8 as the iterator */
-    movi r9, 29  /* r9 holds the value 30 to compare with r8 */
-    movi r12, 0  /*we will use r12 to keep track of the sum*/
+_start: ;annies code
 
-myloop:
-    addi r8, r8, 1  /*increase iterator*/
-    add r12, r12, r8  /*add iterator to the sum*/
-    ble r8, r9, myloop /*loop back if the iterator is less than or equal to 30*/
-.equ    LEDs, 0xFF200000
-    movia r25, LEDs
-    stwio r12, (r25)
+    movi r8, 1 ⁠constant dont change
+    movi r9, 1 #initial value
+    movi r10, 30 #loop until
+    movi r12, 1 
 
-done: br done /*; infinite loop*/
+loop:
+    add r9, r9, r8 #add 1 to r9 store in r9
+    add r12, r12, r9 #add r9 to r9 store in r10
+    blt r9, r10, loop
 
- 
+.equ LEDs, 0xFF200000
+movia r13, LEDs
+stwio r12, (r13)
+done:
+    br done
