b0f9c951b8dd02f604c0b6fa75a29129c909e396
samarqureshii <samarq713@gmail.com>
2024-03-05 14:22:59 -0500

    Part 2 done and working

--- lab7/part2.c
+++ lab7/part2.c
@@ -7,6 +7,8 @@ When the line reaches the top or bottom of the screen, it should start moving in
 
 #include <stdbool.h>
 int pixel_buffer_start;
+#define FPGA_PIXEL_BUF_BASE		0x08000000
+#define FPGA_PIXEL_BUF_END		0x08000000
 
 int main(void){
     volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
@@ -16,39 +18,77 @@ int main(void){
     int y = 238;
 
     //clear the screen
-    for(int y = 0; y <= 239; y++){
-        for(int x = 0; x <= 319; x++){
-            plot_pixel(x,y,0x0000);
-        }
-    }
+    clearScreen();
 
     while(1){
-        //draw the line
-        drawLine(y);
-        //erase the line 
+        //erase the old line 
         eraseLine(y);
 
         //increment position
-        if(direction == 1 && y > 0){ // if we are moving upwards, keep moving upwards
-            y++;
-        }
-
-        else if (direction == -1 && y < 239){ //if we are moving downwards, keep moving downwards
+        if ((direction == 1 && y > 10)) {
             y--;
+        } 
+        
+        else if ((direction == -1 && y < 230)) {
+            y++;
+        } 
+
+        else if(y <= 10 || y >= 230){
+            direction=-direction;
+        }
+        
+        // need for an else state here?
+
+        //draw the line
+        drawLine(y);
+
+        volatile int * pixel_ctrl_ptr = (int *) 0xff203030; // base address
+
+        int status;
+        *pixel_ctrl_ptr = 1; // start the synchronization process
+        // - write 1 into front buffer address register
+        status = *(pixel_ctrl_ptr + 3); // read the status register
+        
+        while ((status & 0x01) != 0){ // polling loop waiting for S bit to go to 0
+            //poll the status register so we know when we are finished writing- after 1/60 of a second 
+            status = *(pixel_ctrl_ptr + 3);
         }
 
-        else if(y == 0 || y == 239){ //we have hit the edge of the screen, toggle the direction
-            direction!=direction;
-        }
+        
 
     }
 }
 
 void drawLine(int y){
     //x positions always stay the same, it is just the y position that is changing
+    //start at x = 79, end at x = 240
+    int colour = 0xFFFF;
+    for(int x = 79; x <=240; x++){
+        plot_pixel(x, y, colour); 
+    }
 }
 
 void eraseLine(int y){
+    int colour = 0x0000;
     //x positions always stay the same, it is just the y position that is changing
+    for(int x = 79; x <=240; x++){
+        plot_pixel(x, y, colour); 
+    }
+}
+
+void plot_pixel(int x, int y, short int line_color){
+    volatile short int *one_pixel_address;
+        
+        one_pixel_address = pixel_buffer_start + (y << 10) + (x << 1);
+        
+        *one_pixel_address = line_color;
+}
+
+void clearScreen(){ //make the screen entirely black
+    for(int y = 0; y <= 239; y++){
+        for(int x = 0; x <= 319; x++){
+            plot_pixel(x,y,0x0000);
+        }
+    }
 
 }
